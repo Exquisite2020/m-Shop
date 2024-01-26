bbf6ff6eefa11ee6b223b56f87d6afb0aec075d6
Exquisite2020 <Exquisitewrite@gmail.com>
2024-01-26 13:10:07 +0100

    e commerce website using firebase

---
+++ src/components/slider/slider-data.jsx
@@ -0,0 +1,28 @@
+export const sliderData = [
+  {
+    id: 1,
+    image: "/assets/sofa.png",
+    heading: "CHAIRS",
+
+    desc: "up to 30% off on all products",
+  },
+  {
+    id: 2,
+    image: "/assets/phone-bg-02.jpg",
+
+    heading: "PHONES",
+    desc: "up to 30% off on all products",
+  },
+  {
+    id: 3,
+    image: "/assets/lappy.png",
+    heading: "LAPTOPS",
+    desc: "up to 30% off on all products",
+  },
+  {
+    id: 4,
+    image: "/assets/wireless-03.png",
+    heading: "HEADSET",
+    desc: "up to 30% off on all products",
+  },
+];
